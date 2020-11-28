5c32734b1209f14f2050dc352bf7532f4495dc14
Ben Ha <benha0898@gmail.com>
2020-11-28 05:20:04 +0000

    create structs file

---
+++ structs.go
@@ -0,0 +1,33 @@
+package main
+
+import "time"
+
+// Room represents a room in which a game takes place
+type Room struct {
+	RoomCode  string   `json:"roomCode,omitempty"`
+	Status    string   `json:"status,omitempty"`
+	FirstTeam string   `json:"firstTeam,omitempty"`
+	Turn      string   `json:"turn,omitempty"`
+	Words     []Word   `json:"words,omitempty"`
+	Players   []Player `json:"players,omitempty"`
+	Logs      []Log    `json:"logs,omitempty"`
+}
+
+// Word represents the 25 words in a game
+type Word struct {
+	Text     string `json:"text,omitempty"`
+	Identity string `json:"identity,omitempty"`
+	Revealed bool   `json:"revealed,omitempty"`
+}
+
+// Player represents a player in a game
+type Player struct {
+	Name string `json:"name,omitempty"`
+	Role string `json:"role,omitempty"`
+}
+
+// Log represents a log message in a game
+type Log struct {
+	Text      string    `json:"text,omitempty"`
+	Timestamp time.Time `json:"timestamp,omitempty"`
+}
