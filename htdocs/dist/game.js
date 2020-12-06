5d71b10890a0cc3f0d3ffb801a8600df63b7cef0
Ben Ha <benha0898@gmail.com>
2020-12-06 13:11:20 +0000

    - Toggle operative/spymaster view - Copy to clipboard functionality - Clickable word tiles that send moves to websocket - Set up onmessage event to update state whenever it receives a message - Minor bux fixes

--- htdocs/dist/game.js
+++ htdocs/dist/game.js
@@ -1,9 +1,21 @@
 "use strict";
+var data;
+var tiles;
+var conn;
 function getGameState() {
     let req = new XMLHttpRequest();
     req.addEventListener("load", function (event) {
-        let data = JSON.parse(req.responseText);
+        data = JSON.parse(req.responseText);
         console.log(data);
+        conn = new WebSocket("ws://" + document.location.host + `/websocket/${data.roomCode}`);
+        console.log("ws://" + document.location.host + "/websocket");
+        conn.onopen = function (evt) {
+            console.log("Connection established.");
+        };
+        conn.onclose = function (evt) {
+            console.log("Connection closed.");
+        };
+        conn.onmessage = updateState;
         updateView(data);
     });
     console.log(window.location.pathname);
@@ -12,19 +24,101 @@ function getGameState() {
 }
 function updateView(data) {
     // Get the templates from the DOM
+    let roomCodeTemplate = $("#room-code-template").html();
     let headerTemplate = $("#game-state-header-template").html();
     let boardTemplate = $("#game-state-board-template").html();
     // Create render functions for the templates with doT.template
+    let roomCodeRenderFunction = doT.template(roomCodeTemplate);
     let headerRenderFunction = doT.template(headerTemplate);
     let boardRenderFunction = doT.template(boardTemplate);
     // Use the render functions to render the data
+    let roomCodeRendered = roomCodeRenderFunction(data);
     let headerRendered = headerRenderFunction(data);
     let boardRendered = boardRenderFunction(data);
     // Insert the rendered results into the DOM
+    $("#room-code").html(roomCodeRendered);
     $("#game-state-header").html(headerRendered);
     $("#game-state-board").html(boardRendered);
+    // update tiles variable
+    tiles = $(".word-tile").toArray();
+    tiles.forEach((tile, index) => {
+        if (!tile.classList.contains("unrevealed")) {
+            tile.removeEventListener("click", () => sendMove(index));
+        }
+        else {
+            tile.addEventListener("click", () => sendMove(index));
+        }
+    });
+}
+function operativeView() {
+    let operativeToggle = $("#operative")[0];
+    let spymasterToggle = $("#spymaster")[0];
+    console.log(!operativeToggle.checked);
+    if (!operativeToggle.checked) {
+        console.log("Switching to operative view");
+        operativeToggle.checked = true;
+        spymasterToggle.checked = false;
+        tiles.forEach((tile, index) => {
+            if (tile.classList.contains(`${data.words[index].identity}-unrevealed`)) {
+                tile.classList.add("unrevealed");
+                tile.classList.remove(`${data.words[index].identity}-unrevealed`);
+                tile.addEventListener("click", () => sendMove(index));
+            }
+        });
+        $("#end-turn-btn").show();
+    }
+    ;
+}
+function spymasterView() {
+    let operativeToggle = $("#operative")[0];
+    let spymasterToggle = $("#spymaster")[0];
+    console.log(!spymasterToggle.checked);
+    if (!spymasterToggle.checked) {
+        console.log("Switching to spymaster view");
+        spymasterToggle.checked = true;
+        operativeToggle.checked = false;
+        tiles.forEach((tile, index) => {
+            if (tile.classList.contains("unrevealed")) {
+                tile.classList.add(`${data.words[index].identity}-unrevealed`);
+                tile.classList.remove("unrevealed");
+                tile.removeEventListener("click", () => sendMove(index));
+            }
+        });
+        $("#end-turn-btn").hide();
+    }
+    ;
+}
+function sendMove(index) {
+    conn.send(data.words[index]);
+}
+function updateState(event) {
+    if (event.data != null) {
+        console.log(event);
+        try {
+            var dataParsed = JSON.parse(event.data);
+            console.log(dataParsed);
+            data = dataParsed;
+            updateView(data);
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+}
+function copyRoomLinkToClipboard() {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(location.host + location.pathname).select();
+    document.execCommand("copy");
+    $temp.remove();
+    $("#copy-btn").focus().popover({
+        trigger: 'focus',
+    });
 }
 $(function () {
     $('[data-toggle="popover"]').popover();
 });
+$("#operative").parent().on("click", operativeView);
+$("#spymaster").parent().on("click", spymasterView);
+$("#copy-btn").on("click", copyRoomLinkToClipboard);
 getGameState();
--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,23 @@
+var data: any;
+var tiles: HTMLElement[];
+var conn: WebSocket;
+
 function getGameState() {
   let req = new XMLHttpRequest();
 
   req.addEventListener("load", function(event) {
-    let data = JSON.parse(req.responseText);
+    data = JSON.parse(req.responseText);
     console.log(data);
+
+    conn = new WebSocket("ws://" + document.location.host + `/websocket/${data.roomCode}`)
+    console.log("ws://" + document.location.host + "/websocket");
+    conn.onopen = function (evt) {
+      console.log("Connection established.");
+    };
+    conn.onclose = function (evt) {
+      console.log("Connection closed.");
+    };
+    conn.onmessage = updateState;
     
     updateView(data);
   });
@@ -15,24 +29,109 @@ function getGameState() {
 
 function updateView(data: any[]) {
   // Get the templates from the DOM
+  let roomCodeTemplate = $("#room-code-template").html();
   let headerTemplate = $("#game-state-header-template").html();
   let boardTemplate = $("#game-state-board-template").html();
 
   // Create render functions for the templates with doT.template
+  let roomCodeRenderFunction = doT.template(roomCodeTemplate);
   let headerRenderFunction = doT.template(headerTemplate);
   let boardRenderFunction = doT.template(boardTemplate);
 
   // Use the render functions to render the data
+  let roomCodeRendered = roomCodeRenderFunction(data);
   let headerRendered = headerRenderFunction(data);
   let boardRendered = boardRenderFunction(data);
 
   // Insert the rendered results into the DOM
+  $("#room-code").html(roomCodeRendered);
   $("#game-state-header").html(headerRendered);
   $("#game-state-board").html(boardRendered);
+
+  // update tiles variable
+  tiles = $(".word-tile").toArray();
+  tiles.forEach((tile, index) => {
+    if (!tile.classList.contains("unrevealed")) {
+      tile.removeEventListener("click", () => sendMove(index));
+    } else {
+      tile.addEventListener("click", () => sendMove(index));
+    }
+  });
+}
+
+function operativeView() {
+  let operativeToggle = $("#operative")[0] as HTMLInputElement;
+  let spymasterToggle = $("#spymaster")[0] as HTMLInputElement;
+  console.log(!operativeToggle.checked);
+  if (!operativeToggle.checked) {
+    console.log("Switching to operative view");
+    operativeToggle.checked = true;
+    spymasterToggle.checked = false;
+    tiles.forEach((tile, index) => {
+      if (tile.classList.contains(`${data.words[index].identity}-unrevealed`)) {
+        tile.classList.add("unrevealed");
+        tile.classList.remove(`${data.words[index].identity}-unrevealed`);
+        tile.addEventListener("click", () => sendMove(index));
+      }
+    });
+    $("#end-turn-btn").show();
+  };
+}
+function spymasterView() {
+  let operativeToggle = $("#operative")[0] as HTMLInputElement;
+  let spymasterToggle = $("#spymaster")[0] as HTMLInputElement;
+  console.log(!spymasterToggle.checked);
+  if (!spymasterToggle.checked) {
+    console.log("Switching to spymaster view");
+    spymasterToggle.checked = true;
+    operativeToggle.checked = false;
+    tiles.forEach((tile, index) => {
+      if (tile.classList.contains("unrevealed")) {
+        tile.classList.add(`${data.words[index].identity}-unrevealed`);
+        tile.classList.remove("unrevealed");
+        tile.removeEventListener("click", () => sendMove(index));
+      }
+    });
+    $("#end-turn-btn").hide();
+  };
+}
+
+function sendMove(index: number) {
+  conn.send(data.words[index]);
+}
+
+function updateState(event: MessageEvent) {
+  if (event.data != null) {
+    console.log(event);
+    try {
+      var dataParsed = JSON.parse(event.data);
+      console.log(dataParsed);
+      data = dataParsed;
+      updateView(data);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+}
+
+function copyRoomLinkToClipboard() {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val(location.host + location.pathname).select();
+  document.execCommand("copy");
+  $temp.remove();
+  
+  (<any>$("#copy-btn")).focus().popover({
+    trigger: 'focus',
+  })
 }
 
 $(function () {
   (<any>$('[data-toggle="popover"]')).popover()
-  })
+})
 
-  getGameState();
+$("#operative").parent().on("click", operativeView);
+$("#spymaster").parent().on("click", spymasterView);
+$("#copy-btn").on("click", copyRoomLinkToClipboard);
+
+getGameState();
